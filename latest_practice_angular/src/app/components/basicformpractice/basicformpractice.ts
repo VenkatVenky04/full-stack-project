1c3e51f989a0eabf9c9fdf88813995f1774875c9
Venkat_04 <gongativenkat04@gmail.com>
2026-05-09 22:09:33 +0530

    reactive form practice..

--- latest_practice_angular/src/app/components/basicformpractice/basicformpractice.ts
+++ latest_practice_angular/src/app/components/basicformpractice/basicformpractice.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-basicformpractice',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './basicformpractice.html',
   styleUrl: './basicformpractice.scss',
 })
 export class Basicformpractice {
 
+  constructor(private fb: FormBuilder) {}
+  basicForm : any;
+  data: any[] = [];
+
+  ngOnInit() {
+    this.basicForm = this.fb.group({
+      name: [''],
+      email: [''],
+      number: ['']
+    });
+
+    const storedData = localStorage.getItem("formData");
+
+    if(storedData) {
+      this.data = JSON.parse(storedData);
+    }
+  }
+
+  onSubmit() {
+    const formData = { ...this.basicForm.value};
+
+    this.data.push(formData);
+
+    localStorage.setItem("formData", JSON.stringify(this.data));
+    this.basicForm.reset();
+
+    console.log("basic form", this.data);
+  }
 }
