a2369347d9f5a0de69fc853a3829cb71e355f743
Venkat_04 <gongativenkat04@gmail.com>
2026-05-14 18:45:26 +0530

    Reactive form practice fixes..

--- latest_practice_angular/src/app/components/reactiveform/reactiveform.ts
+++ latest_practice_angular/src/app/components/reactiveform/reactiveform.ts
@@ -9,12 +9,14 @@ import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 })
 export class reactiveform {
 
-  constructor(private fb: FormBuilder) {}
-  basicForm : any;
+  constructor(private fb: FormBuilder) { }
+  basicForm: any;
   data: any[] = [];
+  editMode: boolean = false;
 
   ngOnInit() {
     this.basicForm = this.fb.group({
+      id: [''],
       name: [''],
       email: [''],
       number: ['']
@@ -22,19 +24,45 @@ export class reactiveform {
 
     const storedData = localStorage.getItem("formData");
 
-    if(storedData) {
+    if (storedData) {
       this.data = JSON.parse(storedData);
     }
   }
 
   onSubmit() {
-    const formData = { ...this.basicForm.value};
+    if (this.editMode) {
+      const index = this.data.findIndex(
+        item => item.id === this.basicForm.value.id
+      );
+      this.data[index] = this.basicForm.value;
+      localStorage.setItem("formData", JSON.stringify(this.data));
+      this.basicForm.reset();
+      this.editMode = false;
+    } else {
+      const formData = {
+        ...this.basicForm.value,
+        id: Date.now()
+      };
+      this.data.push(formData);
+      localStorage.setItem("formData", JSON.stringify(this.data));
+      this.basicForm.reset();
+    }
+  }
 
-    this.data.push(formData);
+  onUpdate(id: number) {
+    console.log("number id", id);
+    const selectedData = this.data.find(
+      item => item.id === id
+    );
 
+    this.basicForm.patchValue(selectedData);
+    this.editMode = true;
+  }
+
+  onDelete(id: number) {
+    this.data = this.data.filter(
+      item => item.id !== id
+    )
     localStorage.setItem("formData", JSON.stringify(this.data));
-    this.basicForm.reset();
-
-    console.log("basic form", this.data);
   }
 }
